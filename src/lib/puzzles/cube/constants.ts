4e6da6d93d6d1ae8dee70fc09bd5453357581780
Connor-Schulz <schulz_connor@yahoo.com>
2025-02-10 18:04:28 -0500

    map the correct side for each relationship

--- src/lib/puzzles/cube/constants.ts
+++ src/lib/puzzles/cube/constants.ts
@@ -25,45 +25,52 @@ export const FaceColors: Record<FaceNames, Colors> = {
   [FaceNames.down]: Colors.orange,
 };
 
+/* Used to define the relationships of adjacent faces relative to a given face */
+export type Relationships = "above" | "below" | "next" | "prev";
+
+/* Used to define the side the adjacent face meets the given face */
+export enum Sides { top, left, bottom, right };
+
+/* Defines the connections of adjacent faces to a provided face */
 export const relationships: Record<
   FaceNames,
-  { above: FaceNames; below: FaceNames; next: FaceNames; prev: FaceNames }
+  { [key in Relationships]: { name: FaceNames; side: Sides } }
 > = {
   [FaceNames.front]: {
-    above: FaceNames.up,
-    below: FaceNames.down,
-    next: FaceNames.right,
-    prev: FaceNames.left,
+    above: { name: FaceNames.up, side: Sides.bottom },
+    next: { name: FaceNames.right, side: Sides.left },
+    below: { name: FaceNames.down, side: Sides.top },
+    prev: { name: FaceNames.left, side: Sides.right },
   },
   [FaceNames.back]: {
-    above: FaceNames.up,
-    below: FaceNames.down,
-    next: FaceNames.left,
-    prev: FaceNames.right,
+    above: { name: FaceNames.up, side: Sides.top },
+    next: { name: FaceNames.left, side: Sides.left },
+    below: { name: FaceNames.down, side: Sides.bottom },
+    prev: { name: FaceNames.right, side: Sides.right },
   },
   [FaceNames.left]: {
-    above: FaceNames.up,
-    below: FaceNames.down,
-    next: FaceNames.front,
-    prev: FaceNames.back,
+    above: { name: FaceNames.up, side: Sides.left },
+    next: { name: FaceNames.front, side: Sides.left },
+    below: { name: FaceNames.down, side: Sides.left },
+    prev: { name: FaceNames.back, side: Sides.right },
   },
   [FaceNames.right]: {
-    above: FaceNames.up,
-    below: FaceNames.down,
-    next: FaceNames.back,
-    prev: FaceNames.front,
+    above: { name: FaceNames.up, side: Sides.right },
+    next: { name: FaceNames.back, side: Sides.left },
+    below: { name: FaceNames.down, side: Sides.right },
+    prev: { name: FaceNames.front, side: Sides.right },
   },
   [FaceNames.up]: {
-    above: FaceNames.back,
-    below: FaceNames.front,
-    next: FaceNames.right,
-    prev: FaceNames.left,
+    above: { name: FaceNames.back, side: Sides.top },
+    next: { name: FaceNames.right, side: Sides.top },
+    below: { name: FaceNames.front, side: Sides.top },
+    prev: { name: FaceNames.left, side: Sides.top },
   },
   [FaceNames.down]: {
-    above: FaceNames.front,
-    below: FaceNames.back,
-    next: FaceNames.right,
-    prev: FaceNames.left,
+    above: { name: FaceNames.front, side: Sides.bottom },
+    next: { name: FaceNames.right, side: Sides.bottom },
+    below: { name: FaceNames.back, side: Sides.bottom },
+    prev: { name: FaceNames.left, side: Sides.bottom },
   },
 };
 
