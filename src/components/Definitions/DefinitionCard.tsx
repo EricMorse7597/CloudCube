b2617ae25ee7d23f852847640cb1595825b0af01
Connor-Schulz <schulz_connor@yahoo.com>
2025-03-03 21:10:41 -0500

    update definition cards to dynamically set

--- src/components/Definitions/DefinitionCard.tsx
+++ src/components/Definitions/DefinitionCard.tsx
@@ -1,29 +1,66 @@
 import {
     Card,
-    HStack,
-    Image,
-    Heading,
+    useColorModeValue,
 } from "@chakra-ui/react";
+
+import styled from "styled-components";
+
+const InfoWrapper = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: flex-start;
+
+    > * {
+        margin: 0 0.5rem;
+    }
+
+    img {
+        margin-left: auto;
+    }
+`;
+
+const TextWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+`;
+
+const Heading = styled.h1`
+    font-size: 1.2rem;
+    font-weight: bold;
+    margin: 0;
+`;
+
+const Description = styled.p`
+    font-size: 1rem;
+    margin: 0;
+`;
+
+const Image = styled.img<{ shadowColor: string }>`
+    width: 75px;
+    aspect-ratio: 1 / 1;
+    justify-content: center;
+    filter: drop-shadow(5px 5px 1px ${props => props.shadowColor});
+`;
 
 export default function DefinitionCard({ label, text, imgHref }: { label: string, text: string, imgHref: string | undefined }) {
     return (
-        <Card p="1.5rem" w="30%" justify={"center"}>
-            <HStack >
-                <div>
-                    <Heading size="md">{label}</Heading>
-                    <p>{text}</p>
-                </div>
-                {imgHref ? (
+        <Card p="1rem" w="30%" justify={"center"} style={{ minWidth: "500px" }}>
+            <InfoWrapper >
+                <TextWrapper>
+                    <Heading>{label}</Heading>
+                    <Description>{text}</Description>
+                </TextWrapper>
+
+                {imgHref && (
                     <Image
-                        ml="auto"
                         src={imgHref}
                         alt={label}
-                        boxSize="75px"
-                        objectFit="contain"
+                        shadowColor={useColorModeValue("#A0AEC0","#1A202C")}
                     />
-                ) : null}
+                )}
 
-            </HStack>
+            </InfoWrapper>
         </Card>
     );
 }
