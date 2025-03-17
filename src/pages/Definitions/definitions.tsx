51cc734c3177b09891fc0e9baf2b2ca84ab85e22
Eric Morse <emorse@uoguelph.ca>
2025-03-17 14:48:21 -0400

    fixed search issue

--- src/pages/Definitions/definitions.tsx
+++ src/pages/Definitions/definitions.tsx
@@ -1,11 +1,11 @@
 import {
-    Box,
-    Container,
-    Flex,
-    Input,
-    Heading,
-    HStack,
-    Stack
+  Box,
+  Container,
+  Flex,
+  Input,
+  Heading,
+  HStack,
+  Stack
 } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import { Divider } from "src/styles/common";
@@ -21,136 +21,136 @@ const Term: Array<DefinitionItem> = [];
 const Competition: Array<DefinitionItem> = [];
 
 const sortItems = () => {
-    DEFINITION_ITEMS.map((item: DefinitionItem) => {
-        switch (item.category) {
-            case category.Move:
-                Move.push(item);
-                break;
-            case category.Method:
-                Method.push(item);
-                break;
-            case category.Algorithm:
-                Algorithm.push(item);
-                break;
-            case category.Term:
-                Term.push(item);
-                break;
-            case category.Competition:
-                Competition.push(item);
-                break;
-        }
-    });
+  DEFINITION_ITEMS.map((item: DefinitionItem) => {
+    switch (item.category) {
+      case category.Move:
+        Move.push(item);
+        break;
+      case category.Method:
+        Method.push(item);
+        break;
+      case category.Algorithm:
+        Algorithm.push(item);
+        break;
+      case category.Term:
+        Term.push(item);
+        break;
+      case category.Competition:
+        Competition.push(item);
+        break;
+    }
+  });
 };
 
 sortItems();
 
 const DefinitionsPage = () => {
-    const [searchBox, setSearch] = useState("");
+  const [searchBox, setSearch] = useState("");
 
-    useEffect(() => {
-        document.title = "Definitions | CloudCube";
-    }, []);
+  useEffect(() => {
+    document.title = "Definitions | CloudCube";
+  }, []);
 
-    return (
-        <div>
-            <Container
-                p={4}>
-                <Input
-                    key={"search"}
-                    placeholder="Search"
-                    onChange={(e) => setSearch(e.target.value)} />
-            </Container>
+  return (
+    <div>
+      <Container
+        p={4}>
+        <Input
+          key={"search"}
+          placeholder="Search"
+          onChange={(e) => setSearch(e.target.value)} />
+      </Container>
 
-            <Stack align={"center"} justify={"center"}>
+      <Stack align={"center"} justify={"center"}>
 
-                <Divider />
-                <Heading>Moves</Heading>
-                <Flex
-                    gap={6}
-                    p={4}
-                    wrap={"wrap"}
-                    justify={"center"}
-                    align={"stretch"}
-                >
+        <Divider />
+        <Heading>Moves</Heading>
+        <Flex
+          gap={6}
+          p={4}
+          wrap={"wrap"}
+          justify={"center"}
+          align={"stretch"}
+        >
 
-                    {Move.map((item: DefinitionItem) => {
-                        if (item.label.includes(searchBox.toUpperCase()) || item.label.includes(searchBox.toLowerCase())) {
-                            return <DefinitionCard key={item.label.replace(" ", "_")} label={item.label} text={item.text} imgHref={item.imgHref} />
-                        }
-                    })}
+          {Move.map((item: DefinitionItem) => {
+            if (item.label.toUpperCase().includes(searchBox.toUpperCase())) {
+              return <DefinitionCard key={item.label.replace(" ", "_")} label={item.label} text={item.text} imgHref={item.imgHref} />
+            }
+          })}
 
-                </Flex >
+        </Flex >
 
-                <Divider />
-                <Heading>Speedcubing Methods & Techniques</Heading>
-                <Flex
-                    gap={6}
-                    p={4}
-                    wrap={"wrap"}
-                    justify={"center"}
-                    align={"stretch"}
-                >
-                    {Method.map((item: DefinitionItem) => {
-                        if (item.label.includes(searchBox.toUpperCase()) || item.label.includes(searchBox.toLowerCase())) {
-                            return <DefinitionCard key={item.label.replace(" ", "_")} label={item.label} text={item.text} imgHref={item.imgHref} />
-                        }
-                    })}
-                </Flex >
+        <Divider />
+        <Heading>Speedcubing Methods & Techniques</Heading>
+        <Flex
+          gap={6}
+          p={4}
+          wrap={"wrap"}
+          justify={"center"}
+          align={"stretch"}
+        >
+          {Method.map((item: DefinitionItem) => {
+            if (item.label.toUpperCase().includes(searchBox.toUpperCase())) {
+              return <DefinitionCard key={item.label.replace(" ", "_")} label={item.label} text={item.text} imgHref={item.imgHref} />
+            }
+          })}
+        </Flex >
 
-                <Divider />
-                <Heading>Common Algorithms & Concepts</Heading>
-                <Flex
-                    gap={6}
-                    p={4}
-                    wrap={"wrap"}
-                    justify={"center"}
-                    align={"stretch"}
-                >
-                    {Algorithm.map((item: DefinitionItem) => {
-                        if (item.label.includes(searchBox.toUpperCase()) || item.label.includes(searchBox.toLowerCase())) {
-                            return <DefinitionCard key={item.label.replace(" ", "_")} label={item.label} text={item.text} imgHref={item.imgHref} />
-                        }
-                    })
-                    }
-                </Flex >
+        <Divider />
+        <Heading>Common Algorithms & Concepts</Heading>
+        <Flex
+          gap={6}
+          p={4}
+          wrap={"wrap"}
+          justify={"center"}
+          align={"stretch"}
+        >
+          {Algorithm.map((item: DefinitionItem) => {
+            if (item.label.toUpperCase().includes(searchBox.toUpperCase())) {
+              return <DefinitionCard key={item.label.replace(" ", "_")} label={item.label} text={item.text} imgHref={item.imgHref} />
+            }
+          })
+          }
+        </Flex >
 
-                <Divider />
-                <Heading>Cubing Terms & Techniques</Heading>
-                <Flex
-                    gap={6}
-                    p={4}
-                    wrap={"wrap"}
-                    justify={"center"}
-                    align={"stretch"}
-                >
-                    {Term.map((item: DefinitionItem) => {
-                        if (item.label.includes(searchBox.toUpperCase()) || item.label.includes(searchBox.toLowerCase())) {
-                            return <DefinitionCard key={item.label.replace(" ", "_")} label={item.label} text={item.text} imgHref={item.imgHref} />
-                        }
-                    })
-                    }
-                </Flex >
+        <Divider />
+        <Heading>Cubing Terms & Techniques</Heading>
+        <Flex
+          gap={6}
+          p={4}
+          wrap={"wrap"}
+          justify={"center"}
+          align={"stretch"}
+        >
+          {Term.map((item: DefinitionItem) => {
+            if (item.label.toUpperCase().includes(searchBox.toUpperCase())) {
+              return <DefinitionCard key={item.label.replace(" ", "_")} label={item.label} text={item.text} imgHref={item.imgHref} />
+            }
+          })
+          }
+        </Flex >
 
-                <Divider />
-                <Heading>Timing & Competition Terms</Heading>
-                <Flex
-                    gap={6}
-                    p={4}
-                    wrap={"wrap"}
-                    justify={"center"}
-                    align={"stretch"}
-                >
-                    {Competition.map((item: DefinitionItem) => {
-                        if (item.label.includes(searchBox.toUpperCase()) || item.label.includes(searchBox.toLowerCase())) {
-                            return <DefinitionCard key={item.label.replace(" ", "_")} label={item.label} text={item.text} imgHref={item.imgHref} />
-                        }
-                    })
-                    }
-                </Flex >
+        <Divider />
+        <Heading>Timing & Competition Terms</Heading>
+        <Flex
+          gap={6}
+          p={4}
+          wrap={"wrap"}
+          justify={"center"}
+          align={"stretch"}
+        >
+          {Competition.map((item: DefinitionItem) => {
+            if (item.label.toUpperCase().includes(searchBox.toUpperCase())) {
+              return <DefinitionCard key={item.label.replace(" ", "_")} label={item.label} text={item.text} imgHref={item.imgHref} />
+            }
+          })
+          }
+        </Flex >
 
-            </Stack>
-        </div >
-    );
+      </Stack>
+    </div >
+  );
 };
 
 export default DefinitionsPage;
